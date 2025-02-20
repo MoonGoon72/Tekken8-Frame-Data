6df48b9a7c1c8cd552ec3e2700540eb0e2e421a3
MoonGoon72 <moongoon.cnu@gmail.com>
2025-02-20 21:54:55 +0900

    chore: 앱 기본 세팅

---
+++ Tekken8 Frame Data/View/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Tekken8 Frame Data
+//
+//  Created by 문영균 on 2/9/25.
+//
+
+import Foundation
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

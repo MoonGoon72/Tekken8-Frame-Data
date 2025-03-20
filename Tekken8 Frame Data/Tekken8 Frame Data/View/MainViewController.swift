7ffe6e944c55abe645fa1b92ba7340a6d1c9d62b
MoonGoon72 <moongoon.cnu@gmail.com>
2025-03-20 15:03:17 +0900

    chore: 필요 파일 추가

---
+++ Tekken8 Frame Data/Tekken8 Frame Data/View/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Tekken8 Frame Data
+//
+//  Created by 문영균 on 3/20/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

1de68a9c71ad0f2b2e4daf569336c7f3c3e64052
MoonGoon72 <moongoon.cnu@gmail.com>
2025-05-27 13:42:19 +0900

    test: unit test 코드 템플릿 작성

---
+++ Tekken8 Frame Data/TK8Tests/MoveListFilteringTests.swift
@@ -0,0 +1,47 @@
+//
+//  MoveListFilteringTests.swift
+//  TK8Tests
+//
+
+import XCTest
+
+final class MoveListFilteringTests: XCTestCase {
+    private let moves: [Move] = [
+        Move(id: 1, characterName: "kazuya", section: "일반", skillNameEN: "Wind God Fist", skillNameKR: "초풍", skillNickname: "초풍", command: "6n23rp", judgment: "상", damage: "24", startupFrame: "11", guardFrame: "+5", hitFrame: "A", counterFrame: "A", attribute: nil, description: nil)
+    ]
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_command_filtering() {
+        // given
+        
+        
+        
+        // when
+        
+        
+        
+        // then
+        
+    }
+    
+    func test_attribute_filtering() {
+        // given
+        
+        
+        
+        // when
+        
+        
+        
+        // then
+        
+    }
+
+
+}

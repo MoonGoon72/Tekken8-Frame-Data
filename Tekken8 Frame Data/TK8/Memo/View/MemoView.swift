35422d7ceaac51227dda903d624dc74fe854670a
MoonGoon72 <moongoon.cnu@gmail.com>
2025-12-09 17:37:56 +0900

    feat: 메모 뷰와 Search Controller 화면만 구현

--- Tekken8 Frame Data/TK8/Memo/View/MemoView.swift
+++ Tekken8 Frame Data/TK8/Memo/View/MemoView.swift
@@ -4,7 +4,47 @@
 //
 
 import Foundation
+import UIKit
 
 final class MemoView: BaseView {
-    
+
+    // MARK: Subviews
+
+    let memoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .tkBackground
+
+        return view
+    }()
+
+    override func setupSubviews() {
+        super.setupSubviews()
+
+        addSubview(memoView)
+    }
+
+    override func setupStyles() {
+        super.setupStyles()
+
+        memoView.backgroundColor = .tkBackground
+    }
+
+    override func setupSubviewLayouts() {
+        super.setupSubviewLayouts()
+        
+        setupMemoViewLayouts()
+    }
+}
+
+private extension MemoView {
+    func setupMemoViewLayouts() {
+        memoView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            memoView.topAnchor.constraint(equalTo: self.topAnchor),
+            memoView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            memoView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            memoView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
 }

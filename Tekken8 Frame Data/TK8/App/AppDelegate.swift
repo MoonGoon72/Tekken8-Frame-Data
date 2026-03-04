4421ccf8a6ac42443717fb244435e211b69ce443
MoonGoon72 <moongoon.cnu@gmail.com>
2026-03-04 17:17:40 +0900

    refactor: Test를 하기 위해 소스 파일을 타겟에 직접 추가 대신 @testable import 로 수정
    - Firebase 모듈 실행을 위해 Test target의 Host Application을 TK8로 추가 및 Host Application's API 호출 허용
    - 기존의 방식은 TK8 앱이 컴파일 되는거 한번, Test 파일에 필요한 소스파일(ex.Memo.swift)가 컴파일 되는게 한번 해서 중복이 발생ㅇ
    - @testable import의 경우 전체를 통틀어 한 번만 컴파일(TK8)이 되고 이를 import 하여 사용하기 때문에 타겟이 늘어나도 한 번만 컴파일하여 시간 절약
    - AppDelegate에서 Test일 때 FirebaseApp.configure() 를 건너뛰도록 설정

--- Tekken8 Frame Data/TK8/App/AppDelegate.swift
+++ Tekken8 Frame Data/TK8/App/AppDelegate.swift
@@ -11,7 +11,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] == nil {
+            FirebaseApp.configure()
+        }
         return true
     }
 

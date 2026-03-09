843882880cfcc7922c992151dadc1decd5714c92
MoonGoon72 <moongoon.cnu@gmail.com>
2026-03-09 14:20:52 +0900

    feat: MemoViewModel 구현
    - ViewModel의 각 메서드가 실행되면 fetch를 통해 저장소와 동기화를 해줘야 했음
    - 따라서 내부 함수로 closure를 사용하는 performAndFetch() 메서드를 두어 fetch 로직을 빼먹지 않도록 함.

---
+++ Tekken8 Frame Data/TK8/Memo/ViewModel/MemoViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  MemoViewModel.swift
+//  TK8
+//
+
+import Combine
+import Foundation
+
+final class MemoViewModel: ObservableObject {
+    @Published private(set) var memos: [Memo] = []
+    private let memoRepository: MemoRepository
+
+    init(memoRepository: MemoRepository) {
+        self.memoRepository = memoRepository
+    }
+
+    func fetch() throws {
+        memos = try memoRepository.fetchMemos()
+    }
+
+    func create(character: String, title: String, body: String) throws {
+        try performAndFetch {
+            try memoRepository.save(character: character, title: title, body: body)
+        }
+    }
+
+    func update(memo: Memo) throws {
+        try performAndFetch {
+            try memoRepository.update(memo: memo)
+        }
+
+    }
+
+    func delete(memos: [Memo]) throws {
+        try performAndFetch {
+            try memos.forEach { try memoRepository.delete(memo: $0) }
+        }
+    }
+
+    private func performAndFetch(_ action: () throws -> Void) throws {
+        try action()
+        try fetch()
+    }
+}
